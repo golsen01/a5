3349a4ee425d39bb33ddc291b8aa58c27e6850e7
Grace Olsen <grace.olsen@tufts.edu>
2013-04-18 22:33:31 -0400

    basic requirements working

--- web.js
+++ web.js
@@ -1,12 +1,99 @@
+// Express initialization
 var express = require('express');
+var app = express(express.logger());
+app.use(express.bodyParser());
+app.set('title', 'nodeapp');
 
-var app = express();//express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('<h1>Hello World!</h1>');
+// Mongo initialization
+var mongoUri = process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/scorecenter';
+var mongo = require('mongodb');
+var db = mongo.Db.connect(mongoUri, function (error, databaseConnection) {
+	db = databaseConnection;
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.get('/', function (request, response) {
+
+	var startString = '<!DOCTYPE HTML><html><head><title>Grace The Scorekeeper</title></head><body><table><tr><th>Game</th><th>User</th><th>Score</th><th>Date</th></tr>';
+	var endString = '</table></body></html>';
+
+	db.collection('highscores', function(err, collection) {
+		collection.find(request.query, function(err, collection){
+			collection.toArray(function(err, collection){
+				for(var i = collection.length - 1; i >=0; i--){
+					if(collection[i].game_title != null){
+						startString += '<tr><td>'+ collection[i].game_title + '</td><td>' + collection[i].username + '</td><td>' + collection[i].score + '</td><td>' + collection[i].datetime + '</td></tr>';
+					}
+				}
+				var compileString = startString + endString;
+
+				response.set('Content-Type', 'text/html');
+				response.send(compileString);
+			});
+		});
+
+	});
+
 });
+
+app.get('/submit.json', function(request, response) {
+	response.header("Access-Control-Allow-Origin", "*");			//these two lines are for CORS
+	response.header("Access-Control-Allow-Headers", "X-Request");
+
+
+	db.collection('highscores', function(err, collection){
+
+		var username = request.query.username;
+		var score = parseInt(request.query.score);
+		var game_title = request.query.game_title;
+		var toInsert = {"username":username, "score":score, "game_title":game_title, "datetime":Date()};
+
+		collection.insert(toInsert, function(err, saved){
+			if (err){
+				response.send("Whoops failure");
+			}
+			else if (!saved){
+				response.send("Not saved");
+			}
+			else{
+				response.send(request.query);
+			}
+		});
+	});
+});
+
+app.get('/highscores.json', function(request, response){
+
+	response.header("Access-Control-Allow-Origin", "*");			//these two lines are for CORS
+	response.header("Access-Control-Allow-Headers", "X-Request");
+
+	db.collection('highscores', function(err, collection){
+		collection.find(request.query, function(err, collection){
+			collection.sort({'score':-1}, function(err, collection){
+				collection.limit(10, function(err, collection){
+					collection.toArray(function(err, collection){
+						var toReturn = '[';
+						for (var i = 0; i < collection.length; i++){
+							toReturn += JSON.stringify(collection[i]);
+							if(i < collection.length - 1){
+								toReturn += ','
+							}
+						}
+						toReturn += ']';
+						response.send(toReturn);
+					});
+				});
+			});
+		});
+	});
+
+});
+
+app.get('/usersearch', function(request, response){
+	response.send('<form name="input" action="/" method="get">Username: <input type="text" name="username"><input type="submit" value="Submit"></form>');
+})
+
+
+// Oh joy! http://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
+app.listen(process.env.PORT || 5000);
